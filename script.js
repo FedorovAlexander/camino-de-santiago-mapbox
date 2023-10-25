82e48ab43bfeca2c0c93d60b742d050b76ffae03
Alexander Fedorov <hamster2240@gmail.com>
2023-10-25 17:22:30 +0300

    Working path animation

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+import flyInAndRotate from './fly-in-and-rotate.js';
+import animatePath from './animate-path.js';
+import { createGeoJSONCircle } from './util.js';
+
 function createMap() {
 	console.log('Page is loaded');
 	const mapboxkey = config.mapbox;
@@ -32,32 +36,87 @@ function createRoute(map, data) {
 			type: 'raster-dem',
 			url: 'mapbox://mapbox.mapbox-terrain-dem-v1',
 			tileSize: 512,
-			maxzoom: 5,
 		});
 		map.setTerrain({ source: 'mapbox-dem', exaggeration: 1.5 });
 		map.setFog({});
-		const pathList = document.getElementById('path-list');
 
-		let newItem = pathList.appendChild(document.createElement('li'));
-		newItem.style.backgroundColor = 'salmon';
-		newItem.classList.add('path-list__item');
-		newItem.innerText = data.features[0].properties.name;
-
-		const coordinates = data.features[0].geometry.coordinates;
-		// start by showing just th	e first coordinate
-		data.features[0].geometry.coordinates = [coordinates[0]];
+		let now = performance.now();
+		mapboxgl.setNow(now);
+		const trackGeojson = data;
 
 		map.addSource('trace', {
 			type: 'geojson',
 			data: data,
 		});
+
+		playAnimations(trackGeojson);
+	});
+
+	const playAnimations = async (trackGeojson) => {
+		return new Promise(async (resolve) => {
+			// add a geojson source and layer for the linestring to the map
+			addPathSourceAndLayer(trackGeojson);
+
+			// get the start of the linestring, to be used for animating a zoom-in from high altitude
+			var targetLngLat = {
+				lng: trackGeojson.features[0].geometry.coordinates[0][0],
+				lat: trackGeojson.features[0].geometry.coordinates[0][1],
+			};
+
+			// animate zooming in to the start point, get the final bearing and altitude for use in the next animation
+			const { bearing, altitude } = await flyInAndRotate({
+				map,
+				targetLngLat,
+				duration: 4000,
+				startAltitude: 3000000,
+				endAltitude: 12000,
+				startBearing: 0,
+				endBearing: -20,
+				startPitch: 40,
+				endPitch: 50,
+			});
+
+			// follow the path while slowly rotating the camera, passing in the camera bearing and altitude from the previous animation
+			await animatePath({
+				map,
+				duration: 10000,
+				path: trackGeojson.features[0],
+				startBearing: bearing,
+				startAltitude: altitude,
+				pitch: 50,
+			});
+
+			// get the bounds of the linestring, use fitBounds() to animate to a final view
+			const bounds = turf.bbox(trackGeojson);
+			map.fitBounds(bounds, {
+				duration: 3000,
+				pitch: 30,
+				bearing: 0,
+				padding: 120,
+			});
+
+			setTimeout(() => {
+				resolve();
+			}, 1000);
+		});
+	};
+
+	const addPathSourceAndLayer = (trackGeojson) => {
+		// Add a line feature and layer. This feature will get updated as we progress the animation
+		map.addSource('line', {
+			type: 'geojson',
+			// Line metrics is required to use the 'line-progress' property
+			lineMetrics: true,
+			data: trackGeojson,
+		});
 		map.addLayer({
+			id: 'line-layer',
 			type: 'line',
-			source: 'trace',
-			id: `line`,
+			source: 'line',
 			paint: {
-				'line-color': 'rgba(255, 0, 0, 0.4)',
-				'line-width': 5,
+				'line-color': 'rgba(0,0,0,0)',
+				'line-width': 9,
+				'line-opacity': 0.8,
 			},
 			layout: {
 				'line-cap': 'round',
@@ -65,75 +124,66 @@ function createRoute(map, data) {
 			},
 		});
 
-		let i = 0;
-		const timer = setInterval(() => {
-			if (i < coordinates.length) {
-				data.features[0].geometry.coordinates.push(coordinates[i]);
-				map.getSource('trace').setData(data);
-				i++;
-			} else {
-				window.clearInterval(timer);
-			}
-		}, 250);
+		map.addSource('start-pin-base', {
+			type: 'geojson',
+			data: createGeoJSONCircle(trackGeojson.features[0].geometry.coordinates[0], 0.04),
+		});
 
-		// animate camera
-		const targetRoute = coordinates;
-		const cameraRoute = coordinates;
-		const animationDuration = 6031000 / 4;
-		const cameraAltitude = 5000;
-		// get the overall distance of each route so we can interpolate along them
-		if (targetRoute.length >= 2) {
-			const routeDistance = turf.lineDistance(turf.lineString(targetRoute));
-			const cameraRouteDistance = turf.lineDistance(turf.lineString(cameraRoute));
+		map.addSource('start-pin-top', {
+			type: 'geojson',
+			data: createGeoJSONCircle(trackGeojson.features[0].geometry.coordinates[0], 0.25),
+		});
 
-			let start;
+		map.addSource('end-pin-base', {
+			type: 'geojson',
+			data: createGeoJSONCircle(trackGeojson.features[0].geometry.coordinates.slice(-1)[0], 0.04),
+		});
 
-			function frame(time) {
-				if (!start) start = time;
-				// phase determines how far through the animation we are
-				const phase = (time - start) / animationDuration;
+		map.addSource('end-pin-top', {
+			type: 'geojson',
+			data: createGeoJSONCircle(trackGeojson.features[0].geometry.coordinates.slice(-1)[0], 0.25),
+		});
 
-				// phase is normalized between 0 and 1
-				// when the animation is finished, reset start to loop the animation
-				if (phase > 1) {
-					// wait 1.5 seconds before looping
-					setTimeout(() => {
-						start = 0.0;
-					}, 1500);
-				}
+		map.addLayer({
+			id: 'start-fill-pin-base',
+			type: 'fill-extrusion',
+			source: 'start-pin-base',
+			paint: {
+				'fill-extrusion-color': '#0bfc03',
+				'fill-extrusion-height': 1000,
+			},
+		});
+		map.addLayer({
+			id: 'start-fill-pin-top',
+			type: 'fill-extrusion',
+			source: 'start-pin-top',
+			paint: {
+				'fill-extrusion-color': '#0bfc03',
+				'fill-extrusion-base': 1000,
+				'fill-extrusion-height': 1200,
+			},
+		});
 
-				// use the phase to get a point that is the appropriate distance along the route
-				// this approach syncs the camera and route positions ensuring they move
-				// at roughly equal rates even if they don't contain the same number of points
-				const alongRoute = turf.along(turf.lineString(targetRoute), routeDistance * phase).geometry.coordinates;
-
-				const alongCamera = turf.along(turf.lineString(cameraRoute), cameraRouteDistance * phase).geometry.coordinates;
-
-				const camera = map.getFreeCameraOptions();
-
-				// set the position and altitude of the camera
-				camera.position = mapboxgl.MercatorCoordinate.fromLngLat(
-					{
-						lng: alongCamera[0],
-						lat: alongCamera[1],
-					},
-					cameraAltitude
-				);
-
-				// tell the camera to look at a point along the route
-				camera.lookAtPoint({
-					lng: alongRoute[0],
-					lat: alongRoute[1],
-				});
-
-				map.setFreeCameraOptions(camera);
-
-				window.requestAnimationFrame(frame);
-			}
-
-			window.requestAnimationFrame(frame);
-		}
-	});
+		map.addLayer({
+			id: 'end-fill-pin-base',
+			type: 'fill-extrusion',
+			source: 'end-pin-base',
+			paint: {
+				'fill-extrusion-color': '#eb1c1c',
+				'fill-extrusion-height': 1000,
+			},
+		});
+		map.addLayer({
+			id: 'end-fill-pin-top',
+			type: 'fill-extrusion',
+			source: 'end-pin-top',
+			paint: {
+				'fill-extrusion-color': '#eb1c1c',
+				'fill-extrusion-base': 1000,
+				'fill-extrusion-height': 1200,
+			},
+		});
+	};
 }
 
 window.onload = createMap;
