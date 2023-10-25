db3dd318ead56498430ca5fae4842b7497a5b866
Alexander Fedorov <hamster2240@gmail.com>
2023-10-25 16:02:39 +0300

    Add helper functions

---
+++ util.js
@@ -0,0 +1,66 @@
+// given a bearing, pitch, altitude, and a targetPosition on the ground to look at,
+// calculate the camera's targetPosition as lngLat
+let previousCameraPosition;
+
+// amazingly simple, via https://codepen.io/ma77os/pen/OJPVrP
+function lerp(start, end, amt) {
+	return (1 - amt) * start + amt * end;
+}
+
+const computeCameraPosition = (pitch, bearing, targetPosition, altitude, smooth = false) => {
+	var bearingInRadian = bearing / 57.29;
+	var pitchInRadian = (90 - pitch) / 57.29;
+
+	var lngDiff = ((altitude / Math.tan(pitchInRadian)) * Math.sin(-bearingInRadian)) / 70000; // ~70km/degree longitude
+	var latDiff = ((altitude / Math.tan(pitchInRadian)) * Math.cos(-bearingInRadian)) / 110000; // 110km/degree latitude
+
+	var correctedLng = targetPosition.lng + lngDiff;
+	var correctedLat = targetPosition.lat - latDiff;
+
+	const newCameraPosition = {
+		lng: correctedLng,
+		lat: correctedLat,
+	};
+
+	if (smooth) {
+		if (previousCameraPosition) {
+			const SMOOTH_FACTOR = 0.95;
+			newCameraPosition.lng = lerp(newCameraPosition.lng, previousCameraPosition.lng, SMOOTH_FACTOR);
+			newCameraPosition.lat = lerp(newCameraPosition.lat, previousCameraPosition.lat, SMOOTH_FACTOR);
+		}
+	}
+
+	previousCameraPosition = newCameraPosition;
+
+	return newCameraPosition;
+};
+
+const createGeoJSONCircle = (center, radiusInKm, points = 64) => {
+	const coords = {
+		latitude: center[1],
+		longitude: center[0],
+	};
+	const km = radiusInKm;
+	const ret = [];
+	const distanceX = km / (111.32 * Math.cos((coords.latitude * Math.PI) / 180));
+	const distanceY = km / 110.574;
+	let theta;
+	let x;
+	let y;
+	for (let i = 0; i < points; i += 1) {
+		theta = (i / points) * (2 * Math.PI);
+		x = distanceX * Math.cos(theta);
+		y = distanceY * Math.sin(theta);
+		ret.push([coords.longitude + x, coords.latitude + y]);
+	}
+	ret.push(ret[0]);
+	return {
+		type: 'Feature',
+		geometry: {
+			type: 'Polygon',
+			coordinates: [ret],
+		},
+	};
+};
+
+export { computeCameraPosition, createGeoJSONCircle };
