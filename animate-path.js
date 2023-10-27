d37844c34b8fc6927eb477b0712cc64a70dda3f5
Alexander Fedorov <hamster2240@gmail.com>
2023-10-27 15:46:55 +0300

    Fix stack overflow problem

--- animate-path.js
+++ animate-path.js
@@ -1,59 +1,40 @@
 import { computeCameraPosition } from './util.js';
 
-const animatePath = async ({ map, duration, path, startBearing, startAltitude, pitch, prod }) => {
-	return new Promise(async (resolve) => {
+const animatePath = ({ map, duration, path, startBearing, startAltitude, pitch }) => {
+	return new Promise((resolve) => {
 		const pathDistance = turf.lineDistance(path);
 		let startTime;
 
-		const frame = async (currentTime) => {
-			if (!startTime) startTime = currentTime;
-			const animationPhase = (currentTime - startTime) / duration;
+		const animate = (timestamp) => {
+			if (!startTime) startTime = timestamp;
+			const animationPhase = (timestamp - startTime) / duration;
 
-			// when the duration is complete, resolve the promise and stop iterating
 			if (animationPhase > 1) {
 				resolve();
-				return;
+			} else {
+				const alongPath = turf.along(path, pathDistance * animationPhase).geometry.coordinates;
+
+				const lngLat = {
+					lng: alongPath[0],
+					lat: alongPath[1],
+				};
+
+				map.setPaintProperty('line-layer', 'line-gradient', ['step', ['line-progress'], 'yellow', animationPhase, 'rgba(0, 0, 0, 0)']);
+
+				const bearing = startBearing - animationPhase * 200.0;
+
+				var correctedPosition = computeCameraPosition(pitch, bearing, lngLat, startAltitude, true);
+
+				const camera = map.getFreeCameraOptions();
+				camera.setPitchBearing(pitch, bearing);
+				camera.position = mapboxgl.MercatorCoordinate.fromLngLat(correctedPosition, startAltitude);
+				map.setFreeCameraOptions(camera);
+
+				requestAnimationFrame(animate);
 			}
-
-			// calculate the distance along the path based on the animationPhase
-			const alongPath = turf.along(path, pathDistance * animationPhase).geometry.coordinates;
-
-			const lngLat = {
-				lng: alongPath[0],
-				lat: alongPath[1],
-			};
-
-			// Reduce the visible length of the line by using a line-gradient to cutoff the line
-			// animationPhase is a value between 0 and 1 that reprents the progress of the animation
-			map.setPaintProperty('line-layer', 'line-gradient', ['step', ['line-progress'], 'yellow', animationPhase, 'rgba(0, 0, 0, 0)']);
-
-			// slowly rotate the map at a constant rate
-			const bearing = startBearing - animationPhase * 200.0;
-
-			// compute corrected camera ground position, so that he leading edge of the path is in view
-			var correctedPosition = computeCameraPosition(
-				pitch,
-				bearing,
-				lngLat,
-				startAltitude,
-				true // smooth
-			);
-
-			// set the pitch and bearing of the camera
-			const camera = map.getFreeCameraOptions();
-			camera.setPitchBearing(pitch, bearing);
-
-			// set the position and altitude of the camera
-			camera.position = mapboxgl.MercatorCoordinate.fromLngLat(correctedPosition, startAltitude);
-
-			// apply the new camera options
-			map.setFreeCameraOptions(camera);
-
-			// repeat!
-			await window.requestAnimationFrame(frame);
 		};
 
-		await window.requestAnimationFrame(frame);
+		requestAnimationFrame(animate);
 	});
 };
 
