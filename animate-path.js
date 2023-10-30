5a135e53fcca01f2b79f56af03d2fc99e06bb4f9
Alexander Fedorov <hamster2240@gmail.com>
2023-10-30 11:53:13 +0200

    Add elevation and progress

--- animate-path.js
+++ animate-path.js
@@ -14,14 +14,18 @@ const animatePath = ({ map, duration, path, startBearing, startAltitude, pitch }
 			} else {
 				const alongPath = turf.along(path, pathDistance * animationPhase).geometry.coordinates;
 				//add a number to the page showing the distance travelled
-				document.getElementById('distance-traveled').innerHTML = (pathDistance * animationPhase).toFixed(1) + ' km';
-				document.getElementById('distance-last').innerHTML = (pathDistance - pathDistance * animationPhase).toFixed(1) + ' km';
+				document.getElementById('distance-traveled').innerHTML = (pathDistance * animationPhase).toFixed(0) + ' km';
+				document.getElementById('distance-last').innerHTML = (pathDistance - pathDistance * animationPhase).toFixed(0) + ' km';
+				document.getElementById('progress').style.width = animationPhase * 100 + '%';
 				const lngLat = {
 					lng: alongPath[0],
 					lat: alongPath[1],
 				};
 
 				map.setPaintProperty('line-layer', 'line-gradient', ['step', ['line-progress'], 'yellow', animationPhase, 'rgba(0, 0, 0, 0)']);
+
+				const elevation = map.queryTerrainElevation(lngLat);
+				document.getElementById('elevation').innerHTML = elevation?.toFixed(0) + ' m';
 
 				const bearing = startBearing - animationPhase * 200.0;
 
